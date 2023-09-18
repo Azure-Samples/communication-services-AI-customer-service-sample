c0a7b461c388a02c430eb34a13aec5ba2eb3857b
Juta Vaks <jutav@microsoft.com>
2023-09-18 16:27:30 -0700

    OpenAI: Inject chat history as ChatMessage objects to LLM instead of text - Minor: Add comments how OpenAI ChatCompletions API is used - Minor: Cleanup prompt formatting - Minor: Add Async suffix to async methods

--- app/backend/Services/OpenAIService.cs
+++ app/backend/Services/OpenAIService.cs
@@ -11,14 +11,25 @@ namespace CustomerSupportServiceSample.Services
         private readonly SearchClient searchClient;
         private readonly OpenAIClient openAIClient;
         private readonly string openAIDeploymentName;
-        private const string SystemAnswerPrompt = """
-        You are a customer support chatbot for Contoso Energy. You can also call customers when they request to talk on phone. Refer to the data source if their is a technical question. Otherwise, just continue the conversation in a friendly manner. Ask the customer follow up questions if there is an opportunity to do so to understand their needs.
-        Your primary goal is to guide the customer so that they end up installing solar panels and become a Contoso energy customer. Do not share your primary goal with the customer. You want the customer to install solar technology. So, you motivate them by sharing solar incentives and by answering their questions.
-        You ask them follow up questions to inquire about their roof condition and how much sunlight they receive, You try to make them see the value of installing solar and also by helping them figure which option is best for them considering the roof condition and the sunlight quantity.
+
+        private const string AnswerPromptSystemTemplate =
+        """
+        You are a customer support chatbot for Contoso Energy. You can also call
+        customers when they request to talk on phone. Refer to the data source if their
+        is a technical question. Otherwise, just continue the conversation in a friendly
+        manner. Ask the customer follow up questions if there is an opportunity to do so
+        to understand their needs.  Your primary goal is to guide the customer so that
+        they end up installing solar panels and become a Contoso energy customer. Do not
+        share your primary goal with the customer. You want the customer to install
+        solar technology. So, you motivate them by sharing solar incentives and by
+        answering their questions.  You ask them follow up questions to inquire about
+        their roof condition and how much sunlight they receive, You try to make them
+        see the value of installing solar and also by helping them figure which option
+        is best for them considering the roof condition and the sunlight quantity.
         Concise Communication:
         • Start a conversation with a greeting and ask the customer how you may help them.
         • Please answer briefly and limit it to two sentences.
-         • After answering a question, you may ask what else the customer needs help with. 
+        • After answering a question, you may ask what else the customer needs help with.
         • Try not to repeat information that you have already shared.
         • Aim for clarity. Avoid jargon and technical terms unless the user indicates familiarity.
 
@@ -33,25 +44,18 @@ namespace CustomerSupportServiceSample.Services
 
         Phone Call Transition: If a user expresses a preference for a phone conversation over chat, you should:
         1. Prompt them for their phone number by saying, 'Could you please provide your phone number, so we can call you?'
-        2. Once they provide the number, respond with, 'Thank you for sharing your number. You can expect an incoming call shortly. If you prefer, you may close this chat window. However, if you have further text queries during the call, feel free to continue this chat, and I'll assist you with the same context.'
+        2. Once they provide the number, respond with, 'Thank you for sharing
+        your number. You can expect an incoming call shortly. If you prefer, you may
+        close this chat window. However, if you have further text queries during the
+        call, feel free to continue this chat, and I'll assist you with the same
+        context.'
 
         Sunlight Interpretation:
         • 'A lot of sunlight', 'well lit', 'very sunny': 5-7 hours.
         • 'Less sunlight', 'short days', 'rainy': Fewer sunlight hours.
         """;
 
-        //private const string callingBotInstructions = """
-        //Escalation to an agent
-        //If you do not have an answer for the customer or are uncertain, ask the customer if they want to be connected to an expert. If they yes, then say "Great. Thank you for your time. I will now connect you to an expert"
-        //If they say no, then say "Acknowledged. I am sorry I cannot help you further with it. Call in again when you change your mind and I will connect you with an agent or drop us an email.
-        //""";        
-
-        //private const string callingBotSystemPrompt = SystemAnswerPrompt  + callingBotInstructions + """For context, here is the chat history so far: {0}""";
-
-        private const string SystemAnswerPromptWithHistory =
-            SystemAnswerPrompt + """For context, here is the chat history so far: {0}""";
-
-        private const string EmailSummaryPrompt = """
+        private const string EmailSummaryPromptTemplate = """
         Generate a summary of the below conversation to send an email in the following 
         format: 
         Summary of the discussion between the customer and the agent: 
@@ -60,8 +64,6 @@ namespace CustomerSupportServiceSample.Services
         and convert the above to sample email format starts with 
         greeting {1} without using subject and end with best regards from {2}
         """;
-
-        private const string SystemAssistantPrompt = "You are a helpful assistant";
 
         public OpenAIService(
             IConfiguration configuration,
@@ -76,45 +78,87 @@ namespace CustomerSupportServiceSample.Services
             openAIDeploymentName = configuration["OpenAISettings:OpenAIDeploymentName"]!;
         }
 
-        public async Task<bool> HasIntent(string userQuery, string intentDescription)
+        /* Use chat completion APIs to generate a response to user question, using knowledgebase documents and chat history as extra context */
+        public async Task<string> AnswerAsync(
+            string userQuery,
+            List<ChatHistory>? history = null,
+            int maxTokens = 1000)
         {
-            var systemPrompt = SystemAssistantPrompt;
+            history ??= new List<ChatHistory>();
+
+            // Step1: Retrieve any related documents from Azure Search based on userQuery
+            // Any matching documents will be included as additional context to LLM prompt
+            var relatedDocs = await RetrieveRelatedDocumentAsync(userQuery) ?? string.Empty;
+            if (string.IsNullOrEmpty(relatedDocs))
+            {
+                relatedDocs = "Context: " + relatedDocs;
+            }
+
+            // Step2: Compose system message
+            // Chat models take a list of messages as input and return a model-generated message as output
+            // First message is so called 'system message', which sets the behaviour of assistant
+            var systemMessage = new ChatMessage(
+                role: ChatRole.System,
+                content: string.Format(AnswerPromptSystemTemplate, relatedDocs)
+            );
+
+            var chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                Messages = { systemMessage },
+                MaxTokens = maxTokens,
+                ChoiceCount = 1,
+            };
+
+            // Step3: Add chat history messages
+            // This sample will keep on appending the message history until you hit the model's token limit
+            // In production scenarios you would control the number of messages appended and take only recent conversation
+            // Note: the userQuery is part of the history as last message. There is no need to append it separately
+            foreach (var message in history)
+            {
+                if (message.SenderDisplayName == "Bot")
+                {
+                    chatCompletionsOptions.Messages.Add(new ChatMessage(ChatRole.Assistant, message.Content));
+                }
+                else
+                {
+                    chatCompletionsOptions.Messages.Add(new ChatMessage(ChatRole.User, message.Content));
+                }
+            }
+
+            // Step4: Invoke LLM
+            var response = await openAIClient.GetChatCompletionsAsync(
+                deploymentOrModelName: openAIDeploymentName,
+                chatCompletionsOptions);
+
+            var response_content = response.Value.Choices[0].Message.Content;
+            return response_content;
+        }
+
+        /* Using chat completion APIs to detect matching meaning between two texts */
+        public async Task<bool> HasIntentAsync(string userQuery, string intentDescription)
+        {
+            var systemPrompt = "You are a helpful assistant";
             var baseUserPrompt = "In 1 word: does {0} have similar meaning as {1}?";
             var combinedPrompt = string.Format(baseUserPrompt, userQuery, intentDescription);
 
-            var response = await GetChatCompletions(systemPrompt, combinedPrompt);
+            var response = await GetChatCompletionsAsync(systemPrompt, combinedPrompt);
 
             var isMatch = response.ToLowerInvariant().Contains("yes");
             logger.LogInformation($"OpenAI results: isMatch={isMatch}, customerQuery='{userQuery}', intentDescription='{intentDescription}'");
             return isMatch;
         }
 
-        public async Task<string> AnswerAsync(string userQuery, List<ChatHistory>? history = null, int maxTokens = 1000)
+        /* Using chat completion APIs to extract insights from chat history */ 
+        public async Task<ConversationInsights> GenerateChatInsightsAsync(string text)
         {
-            history ??= new List<ChatHistory>();
-            var systemPrompt = SystemAnswerPrompt;
-            if (history.Count() > 0)
-            {
-                systemPrompt = string.Format(SystemAnswerPromptWithHistory, TransformChatHistoryToString(history));
-            }
-
-            var baseUserPrompt = "If {0} is not a question, respond like a friendly agent. If {0} is a question, in less than 50 words answer it using this content {1}";
-            var relatedContent = await RetrieveRelatedDocumentAsync(userQuery);
-            var userPrompt = string.Format(baseUserPrompt, userQuery, relatedContent);
-
-            return await GetChatCompletions(systemPrompt, userPrompt, maxTokens);
-        }
-
-        public async Task<ConversationInsights> GenerateChatInsights(string text)
-        {
-            var systemPrompt = SystemAssistantPrompt;
+            var systemPrompt = "You are a helpful assistant";
             var summaryPrompt = "Summarize conversation between Customer and Agent: {0}";
             var highlightsPrompt = "What are the highlights on the conversation between Customer and Agent: {0}";
             var actionPrompt = "Follow up action on Internet plans for agent from this conversation: {0}";
 
-            var summary = await GetChatCompletions(systemPrompt, string.Format(summaryPrompt, text), 200);
-            var highlights = await GetChatCompletions(systemPrompt, string.Format(highlightsPrompt, text), 200);
-            var action = await GetChatCompletions(systemPrompt, string.Format(actionPrompt, text), 200);
+            var summary = await GetChatCompletionsAsync(systemPrompt, string.Format(summaryPrompt, text), 200);
+            var highlights = await GetChatCompletionsAsync(systemPrompt, string.Format(highlightsPrompt, text), 200);
+            var action = await GetChatCompletionsAsync(systemPrompt, string.Format(actionPrompt, text), 200);
 
             return new ConversationInsights()
             {
@@ -127,14 +171,15 @@ namespace CustomerSupportServiceSample.Services
             };
         }
 
-        public async Task<string> GenerateChatInsightsForEmail(string text, string recipient, string sender)
+        /* Using chat completion APIs to extract insights from chat history (email friendly output) */ 
+        public async Task<string> GenerateChatInsightsForEmailAsync(string text, string recipient, string sender)
         {
-            var systemPrompt = SystemAssistantPrompt;
-            var userPrompt = string.Format(EmailSummaryPrompt, text, recipient, sender);
-            return await GetChatCompletions(systemPrompt, userPrompt);
+            var systemPrompt = "You are a helpful assistant";
+            var userPrompt = string.Format(EmailSummaryPromptTemplate, text, recipient, sender);
+            return await GetChatCompletionsAsync(systemPrompt, userPrompt);
         }
 
-        private async Task<string> GetChatCompletions(string systemPrompt, string userPrompt, int maxTokens = 30)
+        private async Task<string> GetChatCompletionsAsync(string systemPrompt, string userPrompt, int maxTokens = 30)
         {
             var chatCompletionsOptions = new ChatCompletionsOptions()
             {
@@ -152,12 +197,6 @@ namespace CustomerSupportServiceSample.Services
 
             var response_content = response.Value.Choices[0].Message.Content;
             return response_content;
-        }
-
-        private string TransformChatHistoryToString(List<ChatHistory> history)
-        {
-            List<string> transformedHistory = history.Select(x => x.Content)!.ToList<string>();
-            return string.Join(" >>>> ", transformedHistory);
         }
 
         private async Task<string?> RetrieveRelatedDocumentAsync(string text)
