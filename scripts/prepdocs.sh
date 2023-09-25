39c80e51e3a717833cea146394bcc23ff1973eb0
Juta Vaks <jutav@microsoft.com>
2023-09-25 07:33:33 -0700

    new updated arch picture

--- scripts/prepdocs.sh
+++ scripts/prepdocs.sh
@@ -1,22 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [ -z "$AZD_PREPDOCS_RAN" ] || [ "$AZD_PREPDOCS_RAN" = "false" ]; then
-    echo 'Running "PrepareDocs.dll"'
+echo 'Running "PrepareDocs.dll"'
 
-    pwd
+pwd
 
-    dotnet run --project "app/prepdocs/PrepareDocs/PrepareDocs.csproj" -- \
-      './data/*.pdf' \
-      --storageconnstring "$AZURE_STORAGE_BLOB_CONNSTRING" \
-      --container "$AZURE_STORAGE_BLOB_CONTAINER" \
-      --searchendpoint "$AZURE_SEARCH_SERVICE_ENDPOINT" \
-      --searchkey  "$AZURE_SEARCH_SERVICE_ENDPOINT_KEY" \
-      --searchindex "$AZURE_SEARCH_INDEX" \
-      --formrecognizerendpoint "$AZURE_AI_SERVICE_ENDPOINT" \
-      --formrecognizerkey "$AZURE_AI_SERVICE_KEY" \
-      -v
-
-    azd env set AZD_PREPDOCS_RAN "true"
-else
-    echo "AZD_PREPDOCS_RAN is set to true. Skipping the run."
-fi
+dotnet run --project "app/prepdocs/PrepareDocs/PrepareDocs.csproj" -- \
+    './data/*.pdf' \
+    --storageconnstring "$AZURE_STORAGE_ACCOUNT_CONNECTIONSTRING" \
+    --container "$AZURE_STORAGE_CONTAINER" \
+    --searchendpoint "$AZURE_SEARCH_SERVICE_ENDPOINT" \
+    --searchkey  "$AZURE_SEARCH_SERVICE_KEY" \
+    --searchindex "$AZURE_SEARCH_INDEX" \
+    --formrecognizerendpoint "$AZURE_AI_SERVICE_ENDPOINT" \
+    --formrecognizerkey "$AZURE_AI_SERVICE_KEY" \
+    -v
